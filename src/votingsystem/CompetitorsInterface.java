f78461d11663db0a55520d6de1a622332b8d8520
fasilminale <fasiladam1290@gmail.com>
2018-12-19 23:16:42 +0300

    implemented DB.java,Server.java, updated interface parameter type,

--- src/votingsystem/CompetitorsInterface.java
+++ src/votingsystem/CompetitorsInterface.java
@@ -4,8 +4,8 @@ import java.rmi.*;
 
 public interface CompetitorsInterface extends Remote{
     
-    public void createCompetitor(String name, int age, String job, String detail) throws RemoteException;
-    public void IncCount() throws RemoteException; //vote
-    public int getCount() throws RemoteException;
+    public void createCompetitor(String name, int age, String job, String detail, int election_id) throws RemoteException;
+    public void IncCount(int competitor_id, int count) throws RemoteException; //vote
+    public int getCount(int competitor_id) throws RemoteException;
     
 }
